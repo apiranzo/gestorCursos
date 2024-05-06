6f150d63d6c289a1ab81f0425c87d4619b51667c
apiranzo <annapejo@gmail.com>
2024-05-06 12:47:40 +0200

    Inicio proyecto

---
+++ gestorCursosApi/src/main/java/com/pejo/anna/gestorCursosApi/GestorCursosApiApplication.java
@@ -0,0 +1,13 @@
+package com.pejo.anna.gestorCursosApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorCursosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorCursosApiApplication.class, args);
+	}
+
+}
---
+++ gestorCursosApi/src/test/java/com/pejo/anna/gestorCursosApi/GestorCursosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pejo.anna.gestorCursosApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorCursosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
